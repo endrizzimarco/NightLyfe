f553cd8a08cb15f6d0333a6cf0c60d140c371a77
Marco Endrizzi <me00531@surrey.ac.uk>
2021-03-21 19:59:27 -0700

    Add GoogleMap component - Basic map styling - Added Markers Icons - Added async await management for APIs

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,130 @@
+<template lang="pug">
+div(ref='map', style='width: 100vw; height: 100vh')
+</template> 
+
+<script>
+import { mapState } from 'vuex'
+import axios from 'axios'
+
+export default {
+  name: 'GoogleMap',
+
+  data() {
+    return {
+      center: null,
+      places: []
+    }
+  },
+
+  methods: {
+    getCurrentPosition(options = {}) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options)
+      })
+    },
+    async geolocate() {
+      if (navigator.geolocation) {
+        try {
+          const position = await this.getCurrentPosition()
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      } else {
+        alert('Geolocation is not supported by this browser.')
+      }
+    },
+
+    async findPlaces() {
+      const URL = `https://secret-ocean-49799.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.center.lat},${this.center.lng}
+                    &radius=1500
+                    &keyword=nightlife
+                    &key=AIzaSyBPUdoB3wV6A9L-H1-J5POiQRmgqqcL9Bk`
+      await axios
+        .get(URL)
+        .then(response => {
+          this.places = response.data.results
+          console.log(this.places)
+        })
+        .catch(error => {
+          console.log(error.message)
+        })
+    },
+
+    async initMap() {
+      var map = new google.maps.Map(this.$refs['map'], {
+        center: new google.maps.LatLng(this.center.lat, this.center.lng),
+        zoom: 15,
+        styles: this.mapStyles,
+        disableDefaultUI: true
+      })
+
+      var infoWindow = new google.maps.InfoWindow()
+
+      this.places.forEach(place => {
+        const lat = place.geometry.location.lat
+        const lng = place.geometry.location.lng
+
+        let marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map,
+          icon: this.getPlaceIcon(place)
+        })
+
+        google.maps.event.addListener(marker, 'click', () => {
+          infoWindow.setContent(
+            `<div class="row nowrap"><div class="col"> ${this.getPlaceImage(place)} ${this.isPlaceOpen(place)}</div>
+            <div class="col-7"> <p class="text-subtitle2">${place.name}</p>
+            <p class="text-weight-light">${place.vicinity}</p> <p>${place.rating} ⭐</p></div></div>`
+          )
+          infoWindow.open(map, marker)
+        })
+      })
+    },
+
+    isPlaceOpen(place) {
+      if (place.opening_hours) {
+        let htmlElement = `<p class="text-negative">Closed</p>`
+        if (place.opening_hours.open_now) {
+          htmlElement = `<p class="text-positive ">Open</p>`
+        }
+        return htmlElement
+      }
+      return ''
+    },
+
+    getPlaceImage(place) {
+      if (place.photos) {
+        const URL =
+          `https://secret-ocean-49799.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=120&key=AIzaSyBPUdoB3wV6A9L-H1-J5POiQRmgqqcL9Bk&photoreference=` +
+          place.photos[0].photo_reference
+        return `<img src="${URL}" height="80" width="60">`
+      }
+      return ''
+    },
+
+    getPlaceIcon(place) {
+      let icon = 'https://img.icons8.com/color/40/000000/disco-ball.png'
+
+      if (place.types[0] == 'bar') {
+        icon = 'https://img.icons8.com/color/40/000000/beer.png'
+      }
+
+      return icon
+    }
+  },
+
+  computed: {
+    ...mapState(['mapStyles'])
+  },
+
+  async mounted() {
+    await this.geolocate()
+    await this.findPlaces()
+    await this.initMap()
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,36 +1,13 @@
 <template lang="pug">
 q-page.flex.flex-center
-  GoogleMap(:api-key='api_key', style='width: 100%; height: 500px', :center='center', :zoom='15')
-    Marker(:options='{ position: center }')
+  GoogleMap
 </template> 
 
 <script>
 import { defineComponent } from 'vue'
-import { GoogleMap, Marker } from 'vue3-google-map'
+import GoogleMap from 'components/GoogleMap.vue'
 
 export default defineComponent({
-  components: { GoogleMap, Marker },
-
-  data() {
-    return {
-      center: null,
-      api_key: process.env.API_KEY
-    }
-  },
-
-  methods: {
-    geolocate() {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-      })
-    }
-  },
-
-  mounted() {
-    this.geolocate()
-  }
+  components: { GoogleMap }
 })
 </script>
