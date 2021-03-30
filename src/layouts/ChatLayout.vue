37bc6e641325832151fa6bdaa2d30ab6fd608d7a
Errikos Kostopoulos <ek00722@surrey.ac.uk>
2021-03-30 14:10:04 -0700

    added chat layout

---
+++ src/layouts/ChatLayout.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+q-layout(view='lHh lpr lff')
+  q-header(elevated)
+    q-toolbar
+      q-btn(
+        v-if='$route.fullPath.includes("/chat")',
+        @click='$router.go(-1)',
+        icon='arrow_back',
+        flat,
+        dense,
+        label='Back'
+      )
+      q-toolbar-title.absolute-center {{ "NightLyfe" }}
+  q-page-container
+    router-view
+</template>
+
+<script>
+</script>
