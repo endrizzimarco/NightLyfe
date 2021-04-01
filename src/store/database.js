6190826e72d816fb0ce7b9e14d1fecc928451cb9
Marco Endrizzi <me00531@surrey.ac.uk>
2021-04-01 09:05:10 -0700

    Restructured Vuex store

---
+++ src/store/database.js
@@ -0,0 +1,17 @@
+import { firebaseAuth, firebaseDb } from 'boot/firebase'
+
+const state = {}
+const mutations = {}
+const actions = {
+  registerUser() {
+    console.log('registerUser')
+  }
+}
+const getters = {}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
---
+++ src/store/menuList.js
@@ -0,0 +1,46 @@
+export default {
+  data: [
+    {
+      icon: 'account_circle',
+      label: 'User',
+      color: 'blue',
+      link: '/userinfo',
+      separator: false
+    },
+    {
+        icon: 'settings',
+        label: 'Settings',
+        color: 'blue-grey-7',
+        link: '',
+        separator: true
+      },
+    {
+        icon: 'help',
+        label: 'Privacy',
+        color: 'accent',
+        link: '',
+        separator: false
+    },
+    {
+      icon: 'error',
+      label: 'Report a Problem',
+      color: 'red-6',
+      link: '',
+      separator: false
+    },
+    {
+      icon: 'feedback',
+      label: 'Send Feedback',
+      color: 'orange',
+      link: '',
+      separator: true
+    },
+    {
+    icon: 'login',
+    label: 'Login',
+    color: 'green',
+    link: '/auth',
+    separator: false
+  },
+  ]
+}
