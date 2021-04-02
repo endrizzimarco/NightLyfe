43c9eafa2c6819d65c6ff2f99e2a4259c0b6c7b6
Marco Endrizzi <me00531@surrey.ac.uk>
2021-04-02 16:19:09 -0700

    Messages are now being stored in the database

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -141,6 +141,13 @@ const actions = {
       messagesRef.off('child_added')
       commit('clearMessages')
     }
+  },
+
+  firebaseSendMessage({}, payload) {
+    firebaseDb.ref('chats/' + state.userDetails.userId + '/' + payload.otherUserId).push(payload.message)
+    
+    payload.message.from = 'them'
+    firebaseDb.ref('chats/' + payload.otherUserId + '/' + state.userDetails.userId).push(payload.message)
   }
 }
 
