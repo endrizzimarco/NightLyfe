fd97b87a408c84ced9af32de79fb0f6d5e8fe379
Henine, Didou (UG - Computer Science) <ah01567@surrey.ac.uk>
2021-04-06 17:40:39 +0200

    edit on the initial paragraphs

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 h4 Privacy: 
 p This section is reserved for our users' privacy !
-p Please, add some privacy option down here for the account security. 
+p Please, add some privacy informations down here that might help securing more your account. 
 br 
 p Contact number: 
 q-input(rounded filled v-model="text" label="Phone number:")
