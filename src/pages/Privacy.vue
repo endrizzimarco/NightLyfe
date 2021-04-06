4728de2ee54be2ac66c896685edf7b9ac9420549
Henine, Didou (UG - Computer Science) <ah01567@surrey.ac.uk>
2021-04-06 21:14:34 +0200

    Added subtitles to the privacy policy

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -2,5 +2,17 @@
 h4 <mark>Privacy policy:</mark> 
 p We recomand our users to read our privacy policy <b>very carfully</b> and contact us if they have any enquiries. 
 p One of our agents will be pleased to answer all of your questions ! 
+p <b>How we use your information ?</b>
+<br>
+p <b>What information do we collect ?</b>
+<br>
+p <b>Our disclosure your information </b>
+<br>
+p <b>Security</b>
+<br>
+p <b>How do we retain your personal information</b>
+<br>
+p <b>Data transfers</b>
+<br>
 
 </template>
