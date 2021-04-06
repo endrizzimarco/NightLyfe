61fab0e3a42ad60cb86c0d01af95d26c06ff7dd5
Henine, Didou (UG - Computer Science) <ah01567@surrey.ac.uk>
2021-04-06 16:22:54 +0200

    Added the phone number input option

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -1,4 +1,14 @@
 <template lang="pug">
-p test
+p Contact number: 
+q-input(rounded filled v-model="text" label="Phone number:")
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
