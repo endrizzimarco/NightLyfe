8f235d461790c4a3458f7b9940de5b7c062feef2
Henine, Didou (UG - Computer Science) <ah01567@surrey.ac.uk>
2021-04-06 21:59:39 +0200

    Added the 'what info do we collect?' paragraph

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -3,10 +3,12 @@ h4 <mark>Privacy policy:</mark>
 p We recomand our users to read our privacy policy <b>very carfully</b> and contact us if they have any enquiries. 
 p One of our agents will be pleased to answer all of your questions ! 
 p <b>How we use your information ?</b>
+p We use personal information in the file we maintain about you, and other information we obtain from your current and past activities on the Application to provide to you the services offered by the Application; resolve any kind of service disputes; troubleshoot problems; measure users' interest in this service; always keep you updated about any live events happening, deliver information to you that, in some cases, is relevant to your interests, such as new bars/nightclubs opening; customize your experience; detect and protect us against errors, fraud and other criminal activity; enforce our Terms of Service; provide you with system or administrative messages. We may also use personal information about you to improve our services, to analyze Application usage, to improve our application content as well as customizing the Application's content, layout, and services if possible. These uses improve the Application and better tailor it to meet your needs, so as to provide you with a smooth, efficient and most importantly safe experience while using our Application.Please see below under “How we Retain your Personal Information” for information on what we do to account information when you terminate your account with us.
 <br>
 p <b>What information do we collect ?</b>
 <br>
-p <b>Our disclosure your information </b>
+p <b>Our disclosure of your information </b>
+p 
 <br>
 p <b>Security</b>
 <br>
