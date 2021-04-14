dc06a1e5c9c59b3e2e79764607aec0d75ded6284
Maya Meylan <mm02479@surrey.ac.uk>
2021-04-14 23:57:38 +0100

    Filled out the form with options

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,20 +1,18 @@
 <template lang="pug">
-h4(class='title') <mark>Privacy Policy:</mark>
-div(class='text')
+h4(class="title") <mark>Privacy Policy:</mark>
+div(class="text")
   p If you have any problems our queries with our app then feel free to contact us using the form below!
-  form
+  form(class="form")
+    label(for="words") Please describe your problem/query here <br>
+    input(type="text", id="words", name="words") <br>
+    p Do you wish to remain anonymous? (this will prevent us from being able to respond) <br>
+    input(type="radio", id="yes", name="anon", value="Y")
+    label(for="yes") Yes <br>
+    input(type="radio", id="no", name="anon", value="N")
+    label(for="no") No <br>
+    input(type="submit" value="Submit")
 <br>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      teal: false,
-    }
-  }
-}
-</script>
 
 <style>
 .title {
