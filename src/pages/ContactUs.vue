9e5f4fe862bc0ea0d1b31cd577e895fd785014f5
Maya Meylan <mm02479@surrey.ac.uk>
2021-04-14 23:10:41 +0100

    Added skeleton form

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,3 +1,27 @@
-<template lang='pug'>
-p test
+<template lang="pug">
+h4(class='title') <mark>Privacy Policy:</mark>
+div(class='text')
+  p If you have any problems our queries with our app then feel free to contact us using the form below!
+  form
+<br>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      teal: false,
+    }
+  }
+}
+</script>
+
+<style>
+.title {
+  text-align:center; 
+}
+.text {
+  border: 3px solid black; 
+  padding: 25px;
+}
+</style>
